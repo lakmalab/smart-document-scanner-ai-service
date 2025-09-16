f3b1c9bbb9c6c1c29a2fae4a6566c792bc6754b4
lakma <lakmalaberathne@gmail.com>
2025-09-16 18:18:39 +0530

    feat added fast api endpoint for ollama

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello")
+from fastapi import FastAPI
+import ollama
+
+app = FastAPI()
+
+@app.post("/generate")
+def generate(prompt: str):
+    response = ollama.chat(model="llama3.2", messages=[{"role":"user","content": prompt}])
+    return {"response": response["message"]["content"]}
