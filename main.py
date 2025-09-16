a3e0b9d2cbb457fe899347563a7ca825ce3d55c1
lakma <lakmalaberathne@gmail.com>
2025-09-16 18:42:28 +0530

    feat added API_KEY authentication and limited the usage

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-from fastapi import FastAPI
+import os
+
+from fastapi import FastAPI, Depends,HTTPException,Header
 import ollama
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY_CREDITS = {os.getenv("API_KEY"): 5}
 
 app = FastAPI()
 
+def verify_api_key(x_api_key: str = Header(None)):
+    credits = API_KEY_CREDITS.get(x_api_key, 0)
+    if credits <= 0:
+        raise HTTPException(status_code=401, detail="Ïnvalid API key, or no credits")
+    return x_api_key
+
 @app.post("/generate")
-def generate(prompt: str):
+def generate(prompt: str, x_api_key: str= Depends(verify_api_key)):
+    API_KEY_CREDITS[x_api_key]-=1
     response = ollama.chat(model="llama3.2", messages=[{"role":"user","content": prompt}])
     return {"response": response["message"]["content"]}
