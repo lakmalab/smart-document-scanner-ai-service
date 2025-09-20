bafbe093093838b81dd9413449b452256228fa4c
lakma <lakmalaberathne@gmail.com>
2025-09-20 17:32:05 +0530

    refactor: structcherd the code in FASTAPI format

--- main.py
+++ main.py
@@ -1,34 +1,11 @@
-import os
 
-from fastapi import FastAPI, Depends,HTTPException,Header
-import ollama
-from dotenv import load_dotenv
-from pydantic import BaseModel
+from fastapi import FastAPI
+from router import router as process_router
 
-load_dotenv()
-
-API_KEY_CREDITS = {os.getenv("API_KEY"): 25}
 
 app = FastAPI()
 
-def verify_api_key(x_api_key: str = Header(None)):
-    credits = API_KEY_CREDITS.get(x_api_key, 0)
-    if credits <= 0:
-        raise HTTPException(status_code=401, detail="Ïnvalid API key, or no credits")
-    return x_api_key
-
-
-
-class PromptRequest(BaseModel):
-    prompt: str
-
-@app.post("/generate")
-def generate(request: PromptRequest, x_api_key: str = Depends(verify_api_key)):
-    API_KEY_CREDITS[x_api_key] -= 1
-    print(request)
-    response = ollama.chat(model="llama3.2", messages=[{"role": "user", "content": request.prompt}])
-    return {response["message"]["content"]}
-
+app.include_router(process_router)
 
 
 #uvicorn main:app --reload
---
+++ test.py
@@ -0,0 +1,36 @@
+'''
+import os
+
+from fastapi import FastAPI, Depends,HTTPException,Header
+import ollama
+from dotenv import load_dotenv
+from pydantic import BaseModel
+
+load_dotenv()
+
+API_KEY_CREDITS = {os.getenv("API_KEY"): 25}
+
+app = FastAPI()
+
+def verify_api_key(x_api_key: str = Header(None)):
+    credits = API_KEY_CREDITS.get(x_api_key, 0)
+    if credits <= 0:
+        raise HTTPException(status_code=401, detail="Ïnvalid API key, or no credits")
+    return x_api_key
+
+
+
+class PromptRequest(BaseModel):
+    prompt: str
+
+@app.post("/generate")
+def generate(request: PromptRequest, x_api_key: str = Depends(verify_api_key)):
+    API_KEY_CREDITS[x_api_key] -= 1
+    print(request)
+    response = ollama.chat(model="llama3.2", messages=[{"role": "user", "content": request.prompt}])
+    return {response["message"]["content"]}
+
+
+
+#uvicorn main:app --reload
+'''
